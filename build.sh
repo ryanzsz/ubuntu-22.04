0b8bb4ee8873c4fc694b6a548c3a4652dedc085f
ryanzsz <63521758+ryanzsz@users.noreply.github.com>
2022-10-28 05:39:22 +0700

    build derp [skip ci]

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ryanzsz/local_manifest --depth 1 -b CR-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_whyred-userdebug
+lunch derp_whyred-userdebug
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=zacky
 export KBUILD_BUILD_HOST=jenkins
 export BUILD_USERNAME=zacky
 export BUILD_HOSTNAME=jenkins
 export ALLOW_MISSING_DEPENDENCIES=true
-export SELINUX_IGNORE_NEVERALLOWS=true
+# export SELINUX_IGNORE_NEVERALLOWS=true
 # export USE_MICROG=true
 # export TARGET_USES_MINI_GAPPS=true
-mka bacon -j8
+mka derp
 # build
