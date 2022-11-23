aa175707aabc52cf09e7c93982b502c82b489a7d
IQ-7 <iq7.dev@gmail.com>
2022-11-23 06:51:14 +0000

    gassss polll

--- build.sh
+++ build.sh
@@ -1,24 +1,27 @@
-#sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-Project/local_manifest --depth 1 -b rosy-SparkOS-13 .repo/local_manifests
+# sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/IQ-7/local_manifest --depth 1 -b rice-13-github .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source $CIRRUS_WORKING_DIR/config
+source $CIRRUS_WORKING_DIR/script/config
 timeStart
 
 source build/envsetup.sh
 export TZ=Asia/Jakarta
-export BUILD_USERNAME=rosy
-export BUILD_HOSTNAME=nfsproject
-lunch spark_rosy-userdebug
+export WITH_GAPPS=true
+export KBUILD_BUILD_USER=zacky
+export KBUILD_BUILD_HOST=android-build
+export BUILD_USERNAME=zacky
+export BUILD_HOSTNAME=android-build
+lunch lineage_whyred-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-mka bacon -j8  > reading #& sleep 95m # Jangan di hapus text line (> reading)
+make bacon -j8  > reading #& sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
 statusBuild
-# end
+# Build
