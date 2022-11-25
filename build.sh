1ae314ed444cc66747f49666b3ac0e84324653e1
ryanzsz <63521758+ryanzsz@users.noreply.github.com>
2022-11-25 07:41:13 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ryanzsz/local_manifest --depth 1 -b CR-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/IQ-7/android -b 10 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ryanzsz/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_whyred-userdebug
+lunch nad_ginkgo-userdebug
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=zacky
 export KBUILD_BUILD_HOST=jenkins
@@ -15,5 +15,5 @@ export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 # export USE_MICROG=true
 # export TARGET_USES_MINI_GAPPS=true
-mka bacon -j8
+make nad -j8
 # build
