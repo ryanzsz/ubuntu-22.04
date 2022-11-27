8ad75565cf99fbc7a1d4a96a6a32f863059f3b82
ryanzsz <63521758+ryanzsz@users.noreply.github.com>
2022-11-28 05:05:50 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/IQ-7/android.git -b 10 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ryanzsz/android_manifest.git -b 10 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ryanzsz/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
