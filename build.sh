e68d221053f8e99466d9f7579f895f6825e3d3b0
ryanzsz <63521758+ryanzsz@users.noreply.github.com>
2022-11-27 15:55:44 +0700

    [skip ci] whyred-NusantaraProject-ROM-10

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/IQ-7/local_manifest --depth 1 -b rice-13-github .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 10 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ryanzsz/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -9,9 +9,6 @@ timeStart
 
 source build/envsetup.sh
 export TZ=Asia/Jakarta
-export WITH_GAPPS=true
-export KBUILD_BUILD_USER=zacky
-export KBUILD_BUILD_HOST=android-build
 export BUILD_USERNAME=zacky
 export BUILD_HOSTNAME=android-build
 lunch lineage_whyred-userdebug
@@ -19,7 +16,7 @@ mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-make bacon -j8  > reading & sleep 95m # Jangan di hapus text line (> reading)
+mka nad -j8  > reading & sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
