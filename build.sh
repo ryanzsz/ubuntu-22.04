bec6eba678aba9ec6cde2aa19f1559e249c1970a
ryanzsz <63521758+ryanzsz@users.noreply.github.com>
2022-11-27 12:26:51 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/IQ-7/android -b 10 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 10 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ryanzsz/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -8,10 +8,10 @@ source build/envsetup.sh
 lunch nad_ginkgo-userdebug
 export TZ=Asia/Jakarta
 export BUILD_USERNAME=zacky
-export BUILD_HOSTNAME=jenkins
+export BUILD_HOSTNAME=android-build
 export ALLOW_MISSING_DEPENDENCIES=true
 # export SELINUX_IGNORE_NEVERALLOWS=true
 # export USE_MICROG=true
 # export TARGET_USES_MINI_GAPPS=true
-make nad -j8
+mka nad -j8
 # build 
