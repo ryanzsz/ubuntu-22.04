de54cb02d4f7ccc80d815d2a8f42253f09c05056
ryanzsz <63521758+ryanzsz@users.noreply.github.com>
2022-12-01 12:37:55 +0700

    bejat

--- build.sh
+++ build.sh
@@ -12,7 +12,6 @@ export TZ=Asia/Jakarta
 export BUILD_USERNAME=zacky
 export BUILD_HOSTNAME=cirrus-ci
 export ALLOW_MISSING_DEPENDENCIES=true
-export SELINUX_IGNORE_NEVERALLOWS=true
 lunch nad_ginkgo-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
