5fd039f5317dcee49c19df7e3dea1e15ae7f29e4
ryanzsz <63521758+ryanzsz@users.noreply.github.com>
2022-12-03 06:55:10 +0700

    Gas tipis tipis

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b ten -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ryanzsz/manifest.git -b ten -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ryanzsz/local_manifest --depth 1 -b havoc-10 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
