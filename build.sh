fbfba7dd9f3d75da4512172a4aac66074672ed29
ryanzsz <63521758+ryanzsz@users.noreply.github.com>
2022-12-03 06:43:34 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 10 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ryanzsz/local_manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b ten -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ryanzsz/local_manifest --depth 1 -b havoc-10 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -12,12 +12,12 @@ export TZ=Asia/Jakarta
 export BUILD_USERNAME=zacky
 export BUILD_HOSTNAME=android-build
 export ALLOW_MISSING_DEPENDENCIES=true
-lunch nad_ginkgo-userdebug
+lunch havoc_ginkgo-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-make nad -j8  > reading #& sleep 95m # Jangan di hapus text line (> reading)
+make bacon -j8  > reading & sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
