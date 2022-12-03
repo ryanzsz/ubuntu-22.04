bc9ef3e2c870f88cfd6e47535e07326384628a44
ryanzsz <63521758+ryanzsz@users.noreply.github.com>
2022-12-03 18:31:19 +0700

    Gasssd

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ryanzsz/local_manifest --depth 1 -b rr-10 .repo/local_manifests
+git clone https://github.com/ryanzsz/local_manifest --depth 1 -b havoc-10 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -10,7 +10,7 @@ timeStart
 source build/envsetup.sh
 export TZ=Asia/Jakarta
 export BUILD_USERNAME=zacky
-export BUILD_HOSTNAME=cirrus-ci
+export BUILD_HOSTNAME=android-build
 lunch rr_ginkgo-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
