ecb42876f154c9fa150c0a02031112a954cdbfd0
ryanzsz <63521758+ryanzsz@users.noreply.github.com>
2022-12-05 23:08:20 +0700

    Update Tipis Tipis

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ source build/envsetup.sh
 export TZ=Asia/Jakarta
 export BUILD_USERNAME=zacky
 export BUILD_HOSTNAME=android-build
+export ALLOW_MISSING_DEPENDENCIES=true
 lunch rr_ginkgo-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
@@ -21,4 +22,4 @@ mka bacon -j8  > reading #& sleep 95m # Jangan di hapus text line (> reading)
 retVal=$?
 timeEnd
 statusBuild
-# end
+# end 
